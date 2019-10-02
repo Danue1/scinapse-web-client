672115b5b0e7a10b0d71a35bfca382ef88f5b4ee
Tyler Shin <shincode@icloud.com>
2019-10-02 11:14:36 +0900

    add options

--- app/components/common/button/button.stories.tsx
+++ app/components/common/button/button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text, select } from '@storybook/addon-knobs';
+import { withKnobs, text, select, boolean } from '@storybook/addon-knobs';
 
 import Button from './index';
 import Icon, { ICONS } from '../../../icons';
@@ -18,6 +18,10 @@ stories
     <Button
       elementType="button"
       size={select('Button size', ['small', 'medium', 'large'], 'medium', 'button-text-only')}
+      variant={select('variant', ['text', 'outlined', 'contained'], 'contained', 'button-text-only')}
+      color={select('text color', ['blue', 'gray', 'black'], 'blue', 'button-text-only')}
+      disabled={boolean('disabled', false, 'button-text-only')}
+      fullWidth={boolean('full width', false, 'button-text-only')}
     >
       {text('Button text', 'Button Text', 'button-text-only')}
     </Button>
@@ -26,8 +30,37 @@ stories
     <Button
       elementType="button"
       size={select('Button size', ['small', 'medium', 'large'], 'medium', 'button-left-icon')}
+      variant={select('variant', ['text', 'outlined', 'contained'], 'contained', 'button-left-icon')}
+      color={select('text color', ['blue', 'gray', 'black'], 'blue', 'button-left-icon')}
+      disabled={boolean('disabled', false, 'button-left-icon')}
+      fullWidth={boolean('full width', false, 'button-left-icon')}
     >
       <Icon icon={select('Icon', Object.keys(ICONS), 'BOOKMARK', 'button-left-icon')} />
       <span>{text('Button text', 'Button Text', 'button-left-icon')}</span>
+    </Button>
+  ))
+  .add('with right icon', () => (
+    <Button
+      elementType="button"
+      size={select('Button size', ['small', 'medium', 'large'], 'medium', 'button-right-icon')}
+      variant={select('variant', ['text', 'outlined', 'contained'], 'contained', 'button-right-icon')}
+      color={select('text color', ['blue', 'gray', 'black'], 'blue', 'button-right-icon')}
+      disabled={boolean('disabled', false, 'button-right-icon')}
+      fullWidth={boolean('full width', false, 'button-right-icon')}
+    >
+      <span>{text('Button text', 'Button Text', 'button-right-icon')}</span>
+      <Icon icon={select('Icon', Object.keys(ICONS), 'BOOKMARK', 'button-right-icon')} />
+    </Button>
+  ))
+  .add('icon only', () => (
+    <Button
+      elementType="button"
+      size={select('Button size', ['small', 'medium', 'large'], 'medium', 'button-icon-only')}
+      variant={select('variant', ['text', 'outlined', 'contained'], 'contained', 'button-icon-only')}
+      color={select('text color', ['blue', 'gray', 'black'], 'blue', 'button-icon-only')}
+      disabled={boolean('disabled', false, 'button-icon-only')}
+      fullWidth={boolean('full width', false, 'button-icon-only')}
+    >
+      <Icon icon={select('Icon', Object.keys(ICONS), 'BOOKMARK', 'button-icon-only')} />
     </Button>
   ));
